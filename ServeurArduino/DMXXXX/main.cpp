2275711d5b5fe890eb2ef70f1de1fdadc40d41d8
LopesDamien <113522401+LopesDamien@users.noreply.github.com>
2024-04-17 15:19:59 +0200

    Update main.cpp

--- ServeurArduino/DMXXXX/main.cpp
+++ ServeurArduino/DMXXXX/main.cpp
@@ -3,70 +3,67 @@
 #include <QSerialPortInfo>
 #include <QDebug>
 
-class SerialPortReader : public QObject
+class ManageArduino : public QObject
 {
     Q_OBJECT
 
 public:
-    explicit SerialPortReader(QObject* parent = nullptr) : QObject(parent) {}
+    explicit ManageArduino(QObject *parent = nullptr) : QObject(parent) {}
 
     void start()
     {
-        // Recherche du port série correspondant à votre Arduino
+        // Recherche du port sÃ©rie correspondant Ã  votre Arduino
         const auto serialPorts = QSerialPortInfo::availablePorts();
-        for (const QSerialPortInfo& serialPort : serialPorts) {
-            if (serialPort.portName() == "COM8") { // Remplacez "COM8" par le port série de votre Arduino
+        for (const QSerialPortInfo &serialPort : serialPorts) {
+            if (serialPort.portName() == "COM8") { // Remplacez "COM8" par le port sÃ©rie de votre Arduino
                 portName = serialPort.portName();
                 break;
             }
         }
 
         if (portName.isEmpty()) {
-            qDebug() << "Aucun port série correspondant à Arduino trouvé.";
+            qDebug() << "Aucun port sÃ©rie correspondant Ã  Arduino trouvÃ©.";
             return;
         }
 
-        // Ouverture du port série
+        // Ouverture du port sÃ©rie
         serial.setPortName(portName);
         serial.setBaudRate(QSerialPort::Baud9600);
         if (!serial.open(QIODevice::ReadOnly)) {
-            qDebug() << "Erreur: Impossible d'ouvrir le port série.";
+            qDebug() << "Erreur: Impossible d'ouvrir le port sÃ©rie.";
             return;
-        }
-        else {
-            qDebug() << "Port série ouvert avec succès.";
+        } else {
+            qDebug() << "Port sÃ©rie ouvert avec succÃ¨s.";
         }
 
-        // Lecture des données de la liaison série
-        QObject::connect(&serial, &QSerialPort::readyRead, this, &SerialPortReader::readData);
+        // Lecture des donnÃ©es de la liaison sÃ©rie
+        QObject::connect(&serial, &QSerialPort::readyRead, this, &ManageArduino::readData);
     }
 
 private slots:
     void readData()
     {
         static QByteArray receivedData;
-        receivedData.append(serial.readAll()); // Ajouter les nouvelles données reçues
+        receivedData.append(serial.readAll()); // Ajouter les nouvelles donnÃ©es reÃ§ues
 
-        // Si les données contiennent un caractère de fin de ligne, traiter la ligne complète
+        // Si les donnÃ©es contiennent un caractÃ¨re de fin de ligne, traiter la ligne complÃ¨te
         if (receivedData.contains('\n')) {
-            QString dataString = QString::fromUtf8(receivedData).trimmed(); // Convertir les données en chaîne
+            QString dataString = QString::fromUtf8(receivedData).trimmed(); // Convertir les donnÃ©es en chaÃ®ne
             if (dataString.startsWith("Joystick direction:")) {
                 qDebug() << dataString;
-            }
-            else {
-                QStringList dataList = dataString.split(':'); // Séparer le numéro et le nom de l'option
+            } else {
+                QStringList dataList = dataString.split(':'); // SÃ©parer le numÃ©ro et le nom de l'option
 
-                // Vérifier que la liste contient au moins deux éléments
+                // VÃ©rifier que la liste contient au moins deux Ã©lÃ©ments
                 if (dataList.size() >= 2) {
                     QString optionNumber = dataList.at(0);
                     QString optionName = dataList.at(1);
-                    qDebug() << "Option sélectionnée :" << optionNumber << " - " << optionName;
-                }
-                else {
-                    qDebug() << "Erreur: Données série mal formattées.";
+                    qDebug() << "Option sÃ©lectionnÃ©e :" << optionNumber << " - " << optionName;
+                } else {
+                    qDebug() << "Erreur: DonnÃ©es sÃ©rie mal formattÃ©es.";
                 }
             }
-            receivedData.clear(); // Effacer les données reçues pour la prochaine lecture
+            receivedData.clear(); // Effacer les donnÃ©es reÃ§ues pour la prochaine lecture
         }
     }
 
@@ -75,12 +72,12 @@ private:
     QString portName;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    SerialPortReader reader;
-    reader.start();
+    ManageArduino manageArduino;
+    manageArduino.start();
 
     return a.exec();
 }
